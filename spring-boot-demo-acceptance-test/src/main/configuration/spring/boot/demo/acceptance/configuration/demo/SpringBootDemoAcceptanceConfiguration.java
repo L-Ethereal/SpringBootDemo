d20290dc74fb00b0590547ae81bdeb4127e4b1be
L-Ethereal <869732789@qq.com>
2019-08-27 22:11:28 +0800

    去掉无用bean

--- spring-boot-demo-acceptance-test/src/main/configuration/spring/boot/demo/acceptance/configuration/demo/SpringBootDemoAcceptanceConfiguration.java
+++ spring-boot-demo-acceptance-test/src/main/configuration/spring/boot/demo/acceptance/configuration/demo/SpringBootDemoAcceptanceConfiguration.java
@@ -5,10 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.grpc.ManagedChannel;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -25,18 +23,10 @@ public class SpringBootDemoAcceptanceConfiguration {
         classpath是在编译后文件存放路径，默认是的bulid/classes/
         lib 和 classes 同属classpath 两者访问的优先级为 lib>classes
      */
-    private ManagedChannel managedChannel;
 
     private final static Logger logger = LoggerFactory.getLogger(SpringBootDemoAcceptanceConfiguration.class);
 
     public SpringBootDemoAcceptanceConfiguration () {
         System.out.println("Start SpringBootDemoAcceptanceConfiguration");
     }
-
-    // 扫描ManagedChannel 方面后面调用
-    @Bean
-    protected ManagedChannel setManagedChannel () {
-        return managedChannel;
-    }
-
 }
